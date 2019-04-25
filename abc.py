45addb86b15fb62839afdf87056a7c8342c633d5
Guanghui Hu <h843219581@outlook.com>
2019-04-25 16:29:59 +0800

    add a class named Hello in abc.py

--- abc.py
+++ abc.py
@@ -1,4 +1,17 @@
 import os, sys
-print('Hello')
-print(os.listdir())
-print(sys.path)
+
+class Hello:
+	def __init__(self, name):
+		self.name=name
+	def waveHand(self):
+		print("{} is waving hands.".format(self.name))
+	def sayHi(self):
+		print("{} is saying hi.".format(self.name))
+
+if __name__=='__main__':
+	print('Hello')
+	print(os.listdir())
+	print(sys.path)
+	h = Hello('Kathy')
+	h.waveHand()
+	h.sayHi()
