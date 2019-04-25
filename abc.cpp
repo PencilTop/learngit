29694221ec2b020ad2e1adb4b95d8b582786e72b
Guanghui Hu <h843219581@outlook.com>
2019-04-25 16:23:14 +0800

    add a multistring function in abc.cpp

--- abc.cpp
+++ abc.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int add(int, int);
+string multistring(string, int);
 
 int main() {
 	int a=10, b=20;
 	cout << "Hello World" << endl;
 	cout << "a + b = " << add(a, b) << endl;
+	multistring("hello", 3);
 	return 0;
 }
 
 
 int add(int a, int b) {
 	return a+b;
+}
+
+string multistring(string s, int i) {
+	while(i > 0) {
+		cout << s << " ";
+		i--;
+	}
+	cout << endl;
 }
