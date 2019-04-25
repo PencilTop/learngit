dec6dd9ebcf83e9ffc5dbdb4dced9e5975c914b0
Guanghui Hu <h843219581@outlook.com>
2019-04-25 10:27:39 +0800

    add distributed

--- abc.cpp
+++ abc.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
+int add(int, int);
 
 int main() {
+	int a=10, b=20;
 	cout << "Hello World" << endl;
+	cout << "a + b = " << add(a, b) << endl;
 	return 0;
+}
+
+
+int add(int a, int b) {
+	return a+b;
 }
