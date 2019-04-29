1afe566ed2ec7ef40b08f19b9f3b2a76e1f794fb
Guanghui Hu <h843219581@outlook.com>
2019-04-29 14:31:03 +0800

    add read_drops method in python script.

--- read_drops.py
+++ read_drops.py
@@ -1,16 +1,31 @@
 #!/bin/env python3
+import re
+
 
 def read_devices(devices_txt):
 	try:
-		with open(devices_txt) as dev:
-			for l in dev.readlines():
+		with open(devices_txt) as devs:
+			for l in devs.readlines():
 				yield l.strip()
 	except Exception as e:
 		print(e)
 		print("File {} not found".format(devices_txt))
 
-
-
+def read_drops(device, pattern):
+	p = re.compile(pattern)
+	try:
+		with open(device) as dev:
+			for l in dev.readlines():
+				m = p.match(device) 
+				if m is not None:
+					yild int(m.group(1))
+	except Exception as e:
+		print(e)
+		print("File {} not found or pattern is wrong".format(device))
 
 if __name__ == '__main__':
 	print(list(read_devices('devices_txt')))
+	drops_pattern = r'.*Total output drops: (\d+)'
+	dropsdic = {}
+	for d in read_devices(devices):
+		dropsdic[d] = list(read_drops(d, drops_pattern))
